bb3ef0f5af6132049ff83bada296a3d7bc79178d
陈暮霭 <926320181@qq.com>
2019-06-20 00:13:01 +0800

    更新

---
+++ components/canvas-drag/canvas-drag.js
@@ -0,0 +1,83 @@
+const defaultOptions = {
+    selector: '#canvas-drag'
+};
+
+function CanvasDrag(options = {}) {
+    options = {
+        ...defaultOptions,
+        ...options,
+    };
+
+    const pages = getCurrentPages();
+    const ctx = pages[pages.length - 1];
+
+  const canvasDrag = ctx.selectComponent(options.selector);
+    delete options.selector;
+
+  return canvasDrag;
+}
+
+CanvasDrag.export = () => {
+  const canvasDrag = CanvasDrag();
+  if (!canvasDrag) {
+        console.error('请设置组件的id="canvas-drag"!!!');
+    } else {
+    return CanvasDrag().export();
+    }
+};
+
+CanvasDrag.initByArr = (arr) => {
+    const canvasDrag = CanvasDrag();
+    if (!canvasDrag) {
+        console.error('请设置组件的id="canvas-drag"!!!');
+    } else {
+        return CanvasDrag().initByArr(arr);
+    }
+};
+
+CanvasDrag.exportJson = () => {
+    const canvasDrag = CanvasDrag();
+    if (!canvasDrag) {
+        console.error('请设置组件的id="canvas-drag"!!!');
+    } else {
+        return CanvasDrag().exportJson();
+    }
+};
+
+CanvasDrag.changFontColor = (color) => {
+    const canvasDrag = CanvasDrag();
+    if (!canvasDrag) {
+        console.error('请设置组件的id="canvas-drag"!!!');
+    } else {
+        return CanvasDrag().changColor(color);
+    }
+};
+
+CanvasDrag.changeBgColor = (color) => {
+    const canvasDrag = CanvasDrag();
+    if (!canvasDrag) {
+        console.error('请设置组件的id="canvas-drag"!!!');
+    } else {
+        return CanvasDrag().changeBgColor(color);
+    }
+};
+
+CanvasDrag.changeBgImage = (bgImage) => {
+    const canvasDrag = CanvasDrag();
+    if (!canvasDrag) {
+        console.error('请设置组件的id="canvas-drag"!!!');
+    } else {
+        return CanvasDrag().changeBgImage(bgImage);
+    }
+};
+
+CanvasDrag.clearCanvas = () => {
+  const canvasDrag = CanvasDrag();
+  if (!canvasDrag) {
+    console.error('请设置组件的id="canvas-drag"!!!');
+  } else {
+    return CanvasDrag().clearCanvas();
+  }
+};
+
+export default CanvasDrag;
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,236 +1,107 @@
-//获取应用实例
-const app = getApp()
+import CanvasDrag from '../../components/canvas-drag/canvas-drag';
 
-let index = 0,
-  items = [], flag = true,
-  itemId = 1;
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    itemList: [],
-    winWidth: 0,
-    winHeight: 0,
-    // tab切换  
-    currentTab: 0
+    graph: {},
+    width: '',
+    height: '',
+    camera: '../../images/shexiang.png',
+    top: 20,
+    left: 20,
+    cameraW: 80,
+    cameraH: 200,
+    addImgList: [],
+    currentIndex: 0,
+    "firstList": ["LXT", "LXT", "LXT", "LXT", "LXT", "LXT"],
+    "secondList": ["GFF", "GFF", "GFF", "GFF", "GFF", "GFF", "GFF", "GFF"]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    var that = this;
-
-    /** 
-     * 获取系统信息 
-     */
-    wx.getSystemInfo({
-      success: function (res) {
-        that.setData({
-          winWidth: res.windowWidth,
-          winHeight: res.windowHeight
-        });
-      }
-    });
-    items = this.data.itemList;
-    this.setDropItem({
-      url: '/images/1.png'
-    });
-    this.setDropItem({
-      url: '/images/1.png'
-    });
-  },
-  setDropItem(imgData) {
-    let data = {},
-      _this = this;
+  onLoad() {
+    // 页面渲染完成  
+    var _this = this;
     wx.getImageInfo({
-      src: imgData.url,
-      success: res => {
-        // 初始化数据
-        data.width = res.width;//宽度
-        data.height = res.height;//高度
-        data.image = imgData.url;//地址
-        data.id = ++itemId;//id
-        data.top = 0;//top定位
-        data.left = 0;//left定位
-        //圆心坐标
-        data.x = data.left + data.width / 2;
-        data.y = data.top + data.height / 2;
-        data.scale = 1;//scale缩放
-        data.oScale = 1;//方向缩放
-        data.rotate = 1;//旋转角度
-        data.active = false;//选中状态
-        console.log(data)
-        items[items.length] = data;
+      src: '../../images/phone.png',
+      success: function(res) {
+        console.log(res)
         _this.setData({
-          itemList: items
+          width: res.width * 2,
+          height: res.height * 2,
         })
       }
     })
   },
-  WraptouchStart: function (e) {
-    for (let i = 0; i < items.length; i++) {
-      items[i].active = false;
-      if (e.currentTarget.dataset.id == items[i].id) {
-        index = i;
-        items[index].active = true;
-      }
-    }
-    this.setData({
-      itemList: items
-    })
-
-    items[index].lx = e.touches[0].clientX;
-    items[index].ly = e.touches[0].clientY;
-
-    console.log(items[index])
+  onReady() {
+    CanvasDrag.changeBgImage('../../images/phone.png');
   },
-  WraptouchMove: function (e) {
-    if (flag) {
-      flag = false;
-      setTimeout(() => {
-        flag = true;
-      }, 100)
-    }
-    // console.log('WraptouchMove', e)
-    items[index]._lx = e.touches[0].clientX;
-    items[index]._ly = e.touches[0].clientY;
-
-    items[index].left += items[index]._lx - items[index].lx;
-    items[index].top += items[index]._ly - items[index].ly;
-    items[index].x += items[index]._lx - items[index].lx;
-    items[index].y += items[index]._ly - items[index].ly;
-
-    items[index].lx = e.touches[0].clientX;
-    items[index].ly = e.touches[0].clientY;
-    console.log(items)
-    this.setData({
-      itemList: items
-    })
-  },
-  oTouchStart: function (e) {
-    //找到点击的那个图片对象，并记录
-    for (let i = 0; i < items.length; i++) {
-      items[i].active = false;
-      if (e.currentTarget.dataset.id == items[i].id) {
-        console.log('e.currentTarget.dataset.id', e.currentTarget.dataset.id)
-        index = i;
-        items[index].active = true;
-      }
-    }
-    //获取作为移动前角度的坐标
-    items[index].tx = e.touches[0].clientX;
-    items[index].ty = e.touches[0].clientY;
-    //移动前的角度
-    items[index].anglePre = this.countDeg(items[index].x, items[index].y, items[index].tx, items[index].ty)
-    //获取图片半径
-    items[index].r = this.getDistancs(items[index].x, items[index].y, items[index].left, items[index].top);
-    console.log(items[index])
-  }
-  ,
-  oTouchMove: function (e) {
-    if (flag) {
-      flag = false;
-      setTimeout(() => {
-        flag = true;
-      }, 100)
-    }
-    //记录移动后的位置
-    items[index]._tx = e.touches[0].clientX;
-    items[index]._ty = e.touches[0].clientY;
-    //移动的点到圆心的距离
-    items[index].disPtoO = this.getDistancs(items[index].x, items[index].y, items[index]._tx, items[index]._ty - 10)
-
-    items[index].scale = items[index].disPtoO / items[index].r;
-    items[index].oScale = 1 / items[index].scale;
-
-    //移动后位置的角度
-    items[index].angleNext = this.countDeg(items[index].x, items[index].y, items[index]._tx, items[index]._ty)
-    //角度差
-    items[index].new_rotate = items[index].angleNext - items[index].anglePre;
-
-    //叠加的角度差
-    items[index].rotate += items[index].new_rotate;
-    items[index].angle = items[index].rotate; //赋值
-
-    //用过移动后的坐标赋值为移动前坐标
-    items[index].tx = e.touches[0].clientX;
-    items[index].ty = e.touches[0].clientY;
-    items[index].anglePre = this.countDeg(items[index].x, items[index].y, items[index].tx, items[index].ty)
-
-    //赋值setData渲染
-    this.setData({
-      itemList: items
-    })
-
-  },
-  getDistancs(cx, cy, pointer_x, pointer_y) {
-    var ox = pointer_x - cx;
-    var oy = pointer_y - cy;
-    return Math.sqrt(
-      ox * ox + oy * oy
-    );
-  },
-  /*
-     *参数1和2为图片圆心坐标
-     *参数3和4为手点击的坐标
-     *返回值为手点击的坐标到圆心的角度
-     */
-  countDeg: function (cx, cy, pointer_x, pointer_y) {
-    var ox = pointer_x - cx;
-    var oy = pointer_y - cy;
-    var to = Math.abs(ox / oy);
-    var angle = Math.atan(to) / (2 * Math.PI) * 360;
-    // console.log("ox.oy:", ox, oy)
-    if (ox < 0 && oy < 0)//相对在左上角，第四象限，js中坐标系是从左上角开始的，这里的象限是正常坐标系  
-    {
-      angle = -angle;
-    } else if (ox <= 0 && oy >= 0)//左下角,3象限  
-    {
-      angle = -(180 - angle)
-    } else if (ox > 0 && oy < 0)//右上角，1象限  
-    {
-      angle = angle;
-    } else if (ox > 0 && oy > 0)//右下角，2象限  
-    {
-      angle = 180 - angle;
-    }
-    return angle;
-  },
-  deleteItem: function (e) {
-    let newList = [];
-    for (let i = 0; i < items.length; i++) {
-      if (e.currentTarget.dataset.id != items[i].id) {
-        newList.push(items[i])
-      }
-    }
-    if (newList.length > 0) {
-      newList[newList.length - 1].active = true;
-    }
-    items = newList;
-    this.setData({
-      itemList: items
-    })
-  },
-  /** 
-     * 滑动切换tab 
-     */
-  bindChange: function (e) {
-    let that = this;
-    that.setData({ currentTab: e.detail.current });
-  },
-  /** 
-   * 点击tab切换 
+  /**
+   * 添加图片
    */
-  swichNav: function (e) {
-    let that = this;
-    if (this.data.currentTab === e.target.dataset.current) {
-      return false;
-    } else {
-      that.setData({
-        currentTab: e.target.dataset.current
+  onAddImage() {
+    let _this = this;
+    wx.chooseImage({
+      success: (res) => {
+        this.setData({
+          graph: {
+            w: 200,
+            h: 200,
+            type: 'image',
+            url: res.tempFilePaths[0],
+          }
+        });
+        CanvasDrag.exportJson()
+          .then((imgArr) => {
+            let addImgList = imgArr.slice(1)
+            _this.setData({
+              addImgList: addImgList
+            })
+          })
+          .catch((e) => {
+            console.error(e);
+          });
+      }
+    })
+  },
+  /**
+   * 导出图片
+   */
+  onExport() {
+    CanvasDrag.export()
+      .then((filePath) => {
+        console.log(filePath);
+        wx.previewImage({
+          urls: [filePath]
+        })
+      })
+      .catch((e) => {
+        console.error(e);
+      })
+
+    CanvasDrag.exportJson()
+      .then((imgArr) => {
+        console.log(imgArr);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  },
+  //swiper切换时会调用
+  pagechange: function(e) {
+    if ("touch" === e.detail.source) {
+      let currentPageIndex = this.data.currentIndex
+      currentPageIndex = (currentPageIndex + 1) % 2
+      this.setData({
+        currentIndex: currentPageIndex
       })
     }
+  },
+  //用户点击tab时调用
+  titleClick: function(e) {
+    let currentPageIndex =
+      this.setData({
+        //拿到当前索引并动态改变
+        currentIndex: e.currentTarget.dataset.idx
+      })
   }
 })
